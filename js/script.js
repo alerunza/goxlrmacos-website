d63067d7b11f3270a6674f27255d98fe7258de40
runzac <91915526+alerunza@users.noreply.github.com>
2023-08-18 17:09:12 +0200

    create script.js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+    fetch('navbar.html')
+        .then(response => response.text())
+        .then(data => {
+            const navbarContainer = document.getElementById('navbar-container');
+            navbarContainer.innerHTML = data;
+        });
+};
