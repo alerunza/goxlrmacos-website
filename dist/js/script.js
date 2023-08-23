b1bd649797c0522dff08e38df3e54fbc732a15d8
runzac <91915526+alerunza@users.noreply.github.com>
2023-08-23 13:32:56 +0200

    add button working to navbarfor mobile devices

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,8 +1,12 @@
 window.onload = function () {
-    fetch('../navbar.html')
+    fetch('navbar.html')
         .then(response => response.text())
         .then(data => {
             const navbarContainer = document.getElementById('navbar-container');
             navbarContainer.innerHTML = data;
         });
 };
+
+function togglehidden() {
+    document.getElementById("mobile-menu").classList.toggle("hidden");
+}
