456da7593d7eb6710ae8a2d656ea5397e4307710
runzac <91915526+alerunza@users.noreply.github.com>
2023-08-24 20:52:15 +0200

    add download (js)

--- dist/js/script.js
+++ dist/js/script.js
@@ -16,3 +16,16 @@ window.onload = function () {
 function togglehidden() {
     document.getElementById("mobile-menu").classList.toggle("hidden");
 }
+
+function downloadAndRedirect() {
+    var link = document.createElement("a");
+    link.href = "https://github.com/AdelaideSky/GoXlr-Macos/releases/download/v2.1.1/GoXlr.App.2.1.1.zip";
+    link.download = "GoXlr.App.2.1.1.zip";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+
+    setTimeout(function() {
+        window.location.href = "download.html";
+    }, 1000);
+}
